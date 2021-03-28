c463e0b03b2ad69c87d4f3a0232d69f2c9a8745e
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-03-28 20:07:33 +0430

    update kafka client | push in events topic

---
+++ kafka_cli.py
@@ -0,0 +1,38 @@
+import enum
+from compiler import Compiler
+from kafka import KafkaConsumer,KafkaProducer
+import json
+import time
+from os import getenv
+
+KAFKA_ENDPOINT = getenv('KAFKA_ENDPOINT')
+
+class Topics(enum.Enum):
+    STORE_CODE = getenv('KAFKA_TOPIC_STORE_CODE')
+    EVENTS = getenv('KAFKA_TOPIC_EVENTS')
+
+consumer = KafkaConsumer(
+    Topics.STORE_CODE,
+    bootstrap_servers=KAFKA_ENDPOINT,
+    group_id=None,
+    auto_offset_reset='earliest',
+    enable_auto_commit=False,
+)
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_ENDPOINT,
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+def get_message():
+    for message in consumer:
+        yield json.loads(message.value.decode("utf-8"))
+        consumer.commit()
+
+def push_event(event) -> bool:
+    try:
+        producer.send(topic=Topics.EVENTS, value=event)
+        producer.flush()
+        return True
+    except Exception as e:
+        print(e)
+        return False
