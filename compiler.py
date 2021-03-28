b604136742e96ea578e89c4144b339024b7c0cbc
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-03-28 19:29:24 +0430

    run compile.sh

--- compiler.py
+++ compiler.py
@@ -1,6 +1,8 @@
 from minio_cli import MinioClient, BucketName
+import subprocess
 
-
+COMPILE_SUCCESS=100
+COMPILE_FAILURE=101
 class Compiler:
 
     @staticmethod
@@ -12,8 +14,14 @@ class Compiler:
         with open('code.zip', 'wb') as f:
             f.write(zip_file)
 
-        ''' compile '''  # todo Arshia
-
+        cmd = subprocess.Popen(["./compile.sh",f"{language}"],stderr=subprocess.DEVNULL,stdout=subprocess.DEVNULL)
+        cmd.communicate()
+        if cmd.returncode == 0:
+            push_event(code_id,COMPILE_SUCCESS,title='code successfully compiled!')
+        else
+            with open('compile.log', 'r') as logfile:
+                push_event(code_id,COMPILE_FAILURE,title='failed to compile code!',logfile.read())
+                return False
         f = open('compiled.zip', 'r')
         file = f.read()
         f.close()
