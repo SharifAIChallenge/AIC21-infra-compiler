90ad1e716813aba62c07045c156e9cc53fe6faa1
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-03-29 00:37:13 +0430

    logging

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import enum
 from compiler import compile
 import kafka_cli as kcli
 
-# import time
-
 while True:
-    # time.sleep(3)
     try:
         data = kcli.get_message()
+        print(f"got new record:{data}")
         event = compile(data['code_id'], data['language'])
+        print(f"resulting event is:{event}")
         kcli.push_event(event)
     except Exception as e:
         print(e)
