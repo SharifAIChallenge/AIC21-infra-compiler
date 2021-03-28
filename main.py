b1fead7ed445e20e528b77f4b92e3560377cc480
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-03-29 00:23:36 +0430

    test

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import enum
-from compiler import compile
-import kafka_cli as kcli
+# import enum
+# from compiler import compile
+# import kafka_cli as kcli
 
+import time
 
 while True:
-    try:
-        data = kcli.get_message()
-        event = compile(data['code_id'], data['language'])
-        kcli.push_event(event)
-    except Exception as e:
-        print(e)
+    time.sleep(3)
+    # try:
+    #     data = kcli.get_message()
+    #     event = compile(data['code_id'], data['language'])
+    #     kcli.push_event(event)
+    # except Exception as e:
+    #     print(e)
