c7e85c2913d6c86c9620e416844cdbf54a987d4c
rezaaminimajd <reza.amini.majd1379@gmail.com>
2021-03-29 20:21:29 +0430

    debug

--- compiler.py
+++ compiler.py
@@ -5,7 +5,7 @@ from event import Event, Event_Status
 
 def download_code(code_id, dest) -> bool:
     zip_file = MinioClient.get_file(code_id, BucketName.Code.value)
-    if zip_file == None:
+    if zip_file is None:
         return False
 
     with open(dest, 'wb') as f:
@@ -25,7 +25,7 @@ def compile(code_id, language) -> Event:
     print(f'receive message with code_id: {code_id}, language: {language}')
 
     source_file = 'code.zip'
-    if download_code(code_id, source_file) == False:
+    if not download_code(code_id, source_file):
         return Event(token_id=code_id, status_code=Event_Status.FILE_NOT_FOUND.value, title='failed to fetch the raw code!')
 
     if __compile(source_file, language) != 0:
@@ -33,11 +33,10 @@ def compile(code_id, language) -> Event:
             return Event(token_id=code_id, status_code=Event_Status.COMPILE_FAILED.value,
                          title='failed to compile code!', message_body=logfile.read())
 
-    with open('bin.zip', 'r') as compiled:
-        bin_zip = compiled.read()
-
-    if MinioClient.upload(code_id, bin_zip, BucketName.Code.value) == False:
-        return Event(token_id=code_id, status_code=Event_Status.UPLOAD_FAILED.value, title='failed to upload the code!')
+    with open('bin.tgz', 'rb') as compiled:
+        if not MinioClient.upload(code_id, compiled, BucketName.Code.value):
+            return Event(token_id=code_id, status_code=Event_Status.UPLOAD_FAILED.value,
+                         title='failed to upload the code!')
 
     return Event(token_id=code_id, status_code=Event_Status.COMPILE_SUCCESS.value,
                  title='code successfully compiled!')
